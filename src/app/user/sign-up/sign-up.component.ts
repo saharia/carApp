598ccfa455fb2b71ef864005f9d583e685e611d0
JR <janakiraman@proisc.com>
2018-06-01 21:14:26 +0530

    user module completed

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user.model';
+declare var customJS: any;
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  public user: User;
+  notifyObj: any;
+  constructor() { 
+    this.notifyObj = new customJS();
+    this.user = new User();
+  }
 
   ngOnInit() {
 
   }
 
-  onSubmit() {
-    
+  onSubmit(event) {
+     console.log(this.notifyObj)
+     this.notifyObj.notify('bottom', 'left', '', 'danger', 'animated fadeInUp', 'animated fadeOutDown', 'Email already exists');
   }
 
 }
